5822083265932aa10dfd431d5a61f423695e96b2
Anand Kunal Mishra <73222935+anandkunalmishra@users.noreply.github.com>
2024-02-05 12:58:18 +0530

    [Anand]: new LinkedList Implementation

---
+++ LinkedList_Implementation/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creation
+            LinkedList<string> linklist = new LinkedList<string>();
+
+            //insert
+            linklist.AddLast("three");
+            linklist.AddFirst("one");
+
+            //iterate
+            LinkedListNode<string>? node = linklist.Find("three");
+
+            if (node != null)
+            {
+                linklist.AddBefore(node,"two");
+                linklist.AddAfter(node, "four");
+            }
+            else
+            {
+                Console.WriteLine("Node not found");
+            }
+
+
+            //print
+            foreach(var item in linklist)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
